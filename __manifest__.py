8bb22db581b7f988693685e0bf8e1dd1ee87f44a
All <alliong75@gmail.com>
2024-04-28 19:18:08 +0700

    video 20
    video 20

--- __manifest__.py
+++ __manifest__.py
@@ -10,11 +10,12 @@
     'sequence' : -100,
     'summary': 'Hospital Management System',
     'description': """Hospital Management System""",
-    'depends': [],
+    'depends': ['mail'],
     'data': [
         'security/ir.model.access.csv',
         'views/menu.xml',
         'views/patient_view.xml',
+        'views/female_patient_view.xml',
     ],
     'demo': [],
     'application': True,
--- models/patient.py
+++ models/patient.py
@@ -4,10 +4,13 @@ from odoo import models, fields, api
 
 class HospitalPatient(models.Model):
     _name = 'hospital.patient'
+    _inherit = ["mail.thread", "mail.activity.mixin"]
     _description = 'Hospital Patient'
 
-    name = fields.Char(string='Name')
+    name = fields.Char(string='Name', tracking=True)
     ref = fields.Char(string='Reference')
-    age = fields.Integer(string='Age')
-    gender = fields.Selection(string='Gender', selection=[('male', 'Male'), ('female', 'Female'),])
+    age = fields.Integer(string='Age', tracking=True)
+    gender = fields.Selection(string='Gender', selection=[('male', 'Male'), ('female', 'Female'),], tracking=True)
+    active = fields.Boolean(string='Active', default=True)
+    
     
