987fd23912c617d0e9f523a3d30763031194d3c6
All <alliong75@gmail.com>
2024-04-30 10:32:14 +0700

    video 26
    Odoo 16

--- models/appointment.py
+++ models/appointment.py
@@ -6,10 +6,15 @@ class HospitalAppointment(models.Model):
     _name = 'hospital.appointment'
     _inherit = ["mail.thread", "mail.activity.mixin"]
     _description = 'Hospital Appointment'
+    _rec_name = 'ref'
 
-    patient_id = fields.Many2one(comodel_name='hospital.patient', string='Appointment')
+    patient_id = fields.Many2one(comodel_name='hospital.patient', string='Patient')
+    gender = fields.Selection(related="patient_id.gender")
     appointment_time = fields.Datetime('Appointment Time', default=fields.Datetime.now)
     booking_date = fields.Date(string='Booking Date', default=fields.Date.context_today)
-    
-    
+    ref = fields.Char(string='Reference')
+   
+    @api.onchange('patient_id')
+    def onchange_patient_id(self):
+        self.ref = self.patient_id.ref
     
